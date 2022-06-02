487a6f068a25b7268e5da76fcc7bde1bb0a13487
MD Ripon Islam <islam876ripon@gmail.com>
2022-06-02 21:32:41 +0600

    user signup complete

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var mongoose = require("mongoose");
 var app = express();
+var bodyParser = require('body-parser')
 var port = process.env.PORT || 5000;
+
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/social-media-backend')
+
+
+
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
 var sl_route =  require('./routes/signup_login');
-
-
-
 app.use(sl_route);
 
 
--- routes/signup_login.js
+++ routes/signup_login.js
@@ -1,6 +1,65 @@
 var express = require("express");
 var router = express.Router();
 var User  = require('../models/user');
+const bcrypt = require('bcrypt');
+
+
+
+
+router.post('/signup',async(req,res)=> {
+
+
+
+if(req.body){
+	var oldUser = await User.find({username : req.body.username});
+
+
+	if(!oldUser){
+
+	try{
+
+		var pwd = await bcrypt.hash(req.body.password,10);
+		var user = new User({
+			username :  req.body.username,
+			password : pwd,
+			first_name : req.body.first_name,
+			last_name : req.body.last_name
+		});
+		// console.log(user);
+		await user.save();
+		res.status(200).json({
+			'msg':  'signup successful'
+		})
+
+	}catch(err){
+
+		console.log(err)
+		res.status(500).json({
+			'err':  'signup failed'
+		})
+
+	}
+	}else{
+
+		res.status(500).json({
+			'err':  'user with the username already exits'
+		})
+	}
+}
+
+
+	
+})
+
+
+
+
+
+
+
+
+
+
 
 
 router.get('/login',(req,res) => {
