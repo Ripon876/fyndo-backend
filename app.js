d49ceb9598d836f93095bf03fd15c378be0efc9f
MD Ripon Islam <islam876ripon@gmail.com>
2022-06-02 22:30:30 +0600

    login complete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require("express");
 var mongoose = require("mongoose");
 var app = express();
--- routes/signup_login.js
+++ routes/signup_login.js
@@ -1,44 +1,40 @@
 var express = require("express");
 var router = express.Router();
 var User  = require('../models/user');
-const bcrypt = require('bcrypt');
-
-
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
 
 router.post('/signup',async(req,res)=> {
-
-
 
 if(req.body){
 	var oldUser = await User.find({username : req.body.username});
 
 
 	if(!oldUser){
+		try{
 
-	try{
+			var pwd = await bcrypt.hash(req.body.password,10);
+			var user = new User({
+				username :  req.body.username,
+				password : pwd,
+				first_name : req.body.first_name,
+				last_name : req.body.last_name
+			});
+			// console.log(user);
+			await user.save();
+			res.status(200).json({
+				'msg':  'signup successful'
+			})
 
-		var pwd = await bcrypt.hash(req.body.password,10);
-		var user = new User({
-			username :  req.body.username,
-			password : pwd,
-			first_name : req.body.first_name,
-			last_name : req.body.last_name
-		});
-		// console.log(user);
-		await user.save();
-		res.status(200).json({
-			'msg':  'signup successful'
-		})
+		}catch(err){
 
-	}catch(err){
+			console.log(err)
+			res.status(500).json({
+				'err':  'signup failed'
+			})
 
-		console.log(err)
-		res.status(500).json({
-			'err':  'signup failed'
-		})
-
-	}
+		}
 	}else{
 
 		res.status(500).json({
@@ -47,23 +43,63 @@ if(req.body){
 	}
 }
 
-
 	
 })
 
 
 
 
-
-
-
-
-
-
-
-
 router.get('/login',(req,res) => {
 	res.send("this is the login route");
+});
+
+
+
+
+
+router.post('/login',async(req,res)=>{
+	
+	if(req.body){
+		try{
+	
+			var user = await User.find({username : req.body.username});
+
+			if(user && user.length > 0){
+				var isPwdValid  = await bcrypt.compare(req.body.password,user[0].password);
+			    
+			    if(isPwdValid){
+			     var token = await jwt.sign({username : user[0].username,id : user[0]._id},process.env.JWT_SECREAT_TOKEN,{ expiresIn: '1h' });
+			    
+			        res.status(200).json({
+			        	'token' : token,
+						'msg':  'login successful'
+					})
+			    
+			    }
+			}
+
+		}catch(err){
+
+			console.log(err)
+			res.status(500).json({
+				'err':  'authentication failed'
+			})
+
+		}
+
+
+
+
+	}else{
+		res.status(401).json({
+			'err':  'username and password is required'
+		})
+	}
 })
+
+
+
+
+
 
 module.exports = router;
