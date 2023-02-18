bfc4b5e995d829a8f63e9d55e07f9b669795e49b
MD Ripon Islam <islam876ripon@gmail.com>
2023-02-18 19:17:56 +0600

    default route res changed

--- app.js
+++ app.js
@@ -1,57 +1,59 @@
-require('dotenv').config();
+require("dotenv").config();
 var express = require("express");
 var mongoose = require("mongoose");
 var app = express();
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var cors = require('cors')
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
 var port = process.env.PORT || 5000;
 var http = require("http");
 var server = http.createServer(app);
-const io = require('./routes/socket').listen(server);
+const io = require("./routes/socket").listen(server);
 
-var mongodbStr = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost:27017/social-media-backend';
-console.log('mongodb uri : ',mongodbStr)
-mongoose.connect(mongodbStr)
-
+var mongodbStr = process.env.MONGODB_URI
+  ? process.env.MONGODB_URI
+  : "mongodb://localhost:27017/social-media-backend";
+console.log("mongodb uri : ", mongodbStr);
+mongoose.connect(mongodbStr);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser('MYMY SECRET SECRET'));
+app.use(cookieParser("MYMY SECRET SECRET"));
 // app.use(cors());
 
-
-
-
-const whitelist = ["http://localhost:3000",'https://social-media99.netlify.app']
+const whitelist = [
+  "http://localhost:3000",
+  "https://social-media99.netlify.app",
+];
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
+      callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"))
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
-}
-app.use(cors(corsOptions))
+};
+app.use(cors(corsOptions));
 
-
-var sl_route =  require('./routes/signup_login');
-var rt_route =  require('./routes/refreshToken');
-var messages = require('./routes/messages');
+var sl_route = require("./routes/signup_login");
+var rt_route = require("./routes/refreshToken");
+var messages = require("./routes/messages");
 app.use(sl_route);
 app.use(rt_route);
 app.use(messages);
 
-
-
-
 app.get("/", (req, res) => {
-  res.send("this is the home route");
+  res.send({
+    status: "ok",
+  });
 });
 
-server.listen(port,()=> {
-    // console.log('server started at port ' + port);
-    console.log('%c server started at port ' + port,'font-weight : bold;color : red;');
+server.listen(port, () => {
+  // console.log('server started at port ' + port);
+  console.log(
+    "%c server started at port " + port,
+    "font-weight : bold;color : red;"
+  );
 });
